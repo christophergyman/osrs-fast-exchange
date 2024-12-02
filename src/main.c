dadacbede25a139eec9f63c4437ff10548878965
christophergyman <christophergayiuman@gmail.com>
2024-12-02 22:04:12 +0000

    wip: cjson practice

--- src/main.c
+++ src/main.c
@@ -3,92 +3,42 @@
 #include <string.h>
 #include <cjson/cJSON.h> 
 
-int create_json(){
-    // create a cJSON object 
-    cJSON *json = cJSON_CreateObject(); 
-    cJSON_AddNumberToObject(json, "id", 11832); 
-    cJSON_AddStringToObject(json, "name", "Bandos Chestplate"); 
-
-    // convert the cJSON object to a JSON string 
-    char *json_str = cJSON_Print(json); 
-
-    // write the JSON string to a file 
-    FILE *fp = fopen("data.json", "w"); 
-    if (fp == NULL) { 
-        printf("Error: Unable to open the file.\n"); 
-        return 1; 
-    } 
-    printf("%s\n", json_str); 
-    fputs(json_str, fp); 
-    fclose;
-    // free the JSON string and cJSON object 
-    cJSON_free(json_str); 
-    cJSON_Delete(json); 
-    return 1;
-}
-
-struct ReturnStruct{
-    int integer_value;
-    char string_value[1024];
-};
-
 // function read json object 
-struct ReturnStruct *read_json(char *filename ,char *request_datatype, char *key){
-    // initialise return struct
-    struct ReturnStruct *return_struct;
-    return_struct.string_value = json_keypair->valuestring;
-
-
+int read_json(char *filename, char *key){
     // open the file 
     FILE *fp = fopen(filename, "r"); 
     if (fp == NULL) { 
         printf("Error: Unable to open the file.\n"); 
         return 1; 
     } 
-  
     // read the file contents into a string 
     char buffer[1024]; 
     int len = fread(buffer, 1, sizeof(buffer), fp); 
     fclose(fp); 
-  
     // put json data into json pointer
     cJSON *json = cJSON_Parse(buffer); 
-
-    char *datatype_string = "string";
-
-    // check type being requested 'string' 'id' 'substring'
-    if (strcmp(request_datatype, datatype_string) == 0 ){
-        printf("[log]: request_datatype: %s \n", datatype_string);
-
-        // access the JSON data "name value"
-        cJSON *json_keypair = cJSON_GetObjectItemCaseSensitive(json, key); 
-        if (cJSON_IsString(json_keypair) && (json_keypair->valuestring != NULL)) { 
-            // printf("json_keypair: %s\n", json_keypair->valuestring); 
-            return_struct.string_value = json_keypair->valuestring;
-            return return_struct;
-        } 
+    if (json == NULL){
+        printf("[error]: failed to parse json \n");
+        return -1;
     }
-  
+    // Get pair from key in the cJson object  
+    cJSON *json_keypair = cJSON_GetObjectItemCaseSensitive(json, key);
 
-    // // access the Id value from the json
-    // cJSON *id = cJSON_GetObjectItemCaseSensitive(json, "id");
-    // printf("ID: %d\n", id->valueint);
-
-    // // access the subitem value from the json
-    // cJSON *subitem = cJSON_GetObjectItemCaseSensitive(json, "sub_item");
-    // cJSON *child_element = subitem->child;
-    // printf("special: %s \n", child_element->valuestring);
-  
-    // // delete the JSON object 
-    // cJSON_Delete(json); 
-    return 0; 
+    // check keypair value and return it
+    if (cJSON_IsString(json_keypair)){
+        printf("it is a string: %s \n", json_keypair->valuestring);
+    }
+    if (cJSON_IsNumber(json_keypair)){
+        printf("it is a integer: %d \n", json_keypair->valueint);
+    }
+    if (cJSON_IsObject(json_keypair)){
+        printf("it is an object: \n");
+    }
 }
 
-int main()
-{
-    char *type = "string";
+int main(){
     char *key = "name";
     char *filename = "data.json";
-    read_json(filename, type, key);
+    read_json(filename, key);
     return 0;
 }
